88bcb53d24392dde11fa3e823e9feefc9d6df9a4
Yashi Gupta <yashig406@gmail.com>
2025-04-07 13:57:47 +0530

    Add PrismJs for syntax highlighting and enhance layout in app component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
 
 function App() {
+  // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    prism.highlightAll();
+  });
+
   return (
     <>
       <main>
         <div className="left">
-          <div className="code"></div>
+          <div className="code">
+            <pre>
+              <code className="language-javascript">
+                {` function sum() {
+                    return 1+1 ;`
+                    }
+              </code>
+            </pre>
+          </div>
 
           <div className="review">Review</div>
         </div>
