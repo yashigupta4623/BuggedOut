a67c5dbfc064a66624505b1aa6929837509fa36d
Yashi Gupta <yashig406@gmail.com>
2025-04-07 15:36:56 +0530

    Add CORS support and integrate Axios for code review feature in frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes.js');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // testing route :
 app.get('/', (req, res) =>{
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Editor from "react-simple-code-editor";
 import "prismjs/themes/prism-tomorrow.css";
+import axios from "axios";
 import prism from "prismjs";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [code, setCode] = useState(
+    `function sum() {
+      return 1+1 ;
+    }`
+  );
+
+  const [review, setReview] = useState("");
 
   useEffect(() => {
     prism.highlightAll();
   });
+
+async function reviewCode() {
+    axios
+      .post("http://localhost:3000/ai/get-review", { code })
+      .then((response) => {
+        setReview(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   return (
     <>
       <main>
         <div className="left">
           <div className="code">
-            <pre>
-              <code className="language-javascript">
-                {` function sum() {
-                    return 1+1 ;`
-                    }
-              </code>
-            </pre>
-          </div>
-
-          <div className="review">Review</div>
+              <Editor
+                value={code}
+                onValueChange={(code) => setCode(code)}
+                highlight={(code) => prism.highlight(code, prism.languages.js)}
+                padding={10}
+                style={{
+                  fontFamily: '"Fira code", "Fira Mono", monospace',
+                  fontSize: 16,
+                  border: "1px solid #ffffff",
+                  borderRadius: "5px",
+                  height: "100%",
+                  width: "100%",
+                }}
+              />
+            </div>
+          <div className="review" onClick={reviewCode}>Review</div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          {review}
+        </div>
       </main>
     </>
   );
