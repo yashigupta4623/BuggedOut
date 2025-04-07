54b1d668a9c9e6888cadd39ea35cc3a656538af3
Yashi Gupta <yashig406@gmail.com>
2025-04-07 12:41:04 +0530

    initialize React project with Vite setup and basic styling

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code"></div>
+
+          <div className="review">Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
